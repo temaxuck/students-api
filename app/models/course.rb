85f68e57207d18275f23f5497cd1d7199ff49bbc
temaxuck <gorropand@gmail.com>
2024-02-25 10:46:32 +0300

    Add `Student` and `Course` validation

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,7 @@
 class Course < ApplicationRecord
   has_many :students
+
+  validates :number, presence: true, inclusion: 1..11
+  validates :letter, presence: true, length: { maximum: 1 }
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,8 @@
 class Student < ApplicationRecord
   belongs_to :course, counter_cache: :students_count
+
+  validates :first_name, presence: true, length: { minimum: 1 }
+  validates :last_name, presence: true, length: { minimum: 1 }
+  validates :school_id, presence: true, numericality: { greater_than_or_equal_to: 1, only_integer: true }
+  
 end
