97b0e4dd43a7705d64c2cb227a9a291df55caafe
temaxuck <gorropand@gmail.com>
2024-02-25 19:20:38 +0300

    Write tests for course model

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 class CourseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @school = schools(:one)
+  end
+
+  test "should not save course without required fields" do
+    course = @school.courses.new
+    assert_not course.save, "Saved course without required fields"
+  end
+
+  test "should not save course with incorrect number" do
+    course = @school.courses.new(letter: "A")
+    assert_not course.save, "Saved course without specified number"
+
+    course_number = 0
+    course = @school.courses.new(number: course_number, letter: "A")
+    assert_not course.save, "Saved course with incorrect course number: %d" % course_number
+
+    course_number = 12
+    course = @school.courses.new(number: course_number, letter: "A")
+    assert_not course.save, "Saved course with incorrect course number: %d" % course_number
+  end
+
+  test "should not save course with incorrect letter" do
+    course_letter = "not-a-letter"
+    course = @school.courses.new(number: 1, letter: course_letter)
+    assert_not course.save, "Saved course with letter which length is not equal to 1: %s" % course_letter
+  end
+
 end
