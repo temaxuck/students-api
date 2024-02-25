1c25a256bc30e306e0871c66b7dbbff2677cc2d1
temaxuck <gorropand@gmail.com>
2024-02-25 17:02:45 +0300

    Fix bug with showing wrong students for a course

--- app/controllers/classes_controller.rb
+++ app/controllers/classes_controller.rb
@@ -7,7 +7,7 @@ class ClassesController < ApplicationController
 
   def show
     @school = School.find(params[:school_id])
-    @course = @school.courses.find(params[:school_id])
+    @course = @school.courses.find(params[:class_id])
     @students = @course.students
 
     render json: {data: @students.map(&:students_json_builder)}, status: :ok
