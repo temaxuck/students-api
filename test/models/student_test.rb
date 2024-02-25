b4b2cdded45aeebb1a84c9c672d0d719aa677858
temaxuck <gorropand@gmail.com>
2024-02-25 20:00:23 +0300

    Add tests for Student model

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class StudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @course = courses(:one)
+  end
+
+  test "should not save course without required fields" do
+    student = @course.students.new
+    assert_not student.save, "Saved student without required fields"
+  end
+
+  test "should save student with empty last name" do
+    student = @course.students.new(first_name: "test-first-name", surname: "test-surname")
+    assert student.save, "Didn't save student without specified last name"
+  end
+
+  test "created student should have secret_salt field" do
+    student = @course.students.new(first_name: "test-first-name", surname: "test-surname")
+    assert student.has_attribute?(:secret_salt), "Created student instance doesn't have secret_salt attribute"
+  end
+
 end
