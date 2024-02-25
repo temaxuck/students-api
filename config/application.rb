e7d37f30237b0cec9f4408c331c14eaae6e58e05
temaxuck <gorropand@gmail.com>
2024-02-25 16:14:05 +0300

    Edit app's config

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,8 @@ module StudentsApi
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Moscow"
+    config.active_record.default_timezone = :local 
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Only loads a smaller set of middleware suitable for API only apps.
