ad67200bf9312ee97140035339b0ba89ab416002
temaxuck <gorropand@gmail.com>
2024-02-25 20:44:58 +0300

    Add tests for classes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  post "students", to: "students#create"
-  delete 'students/:id', to: "students#destroy"
-  get 'schools/:school_id/classes', to: "classes#index"
-  get 'schools/:school_id/classes/:class_id/students', to: "classes#show"
+  post "students", to: "students#create", as: "students_create"
+  delete 'students/:id', to: "students#destroy", as: "students_destroy"
+  get 'schools/:school_id/classes', to: "classes#index", as: "classes_index"
+  get 'schools/:school_id/classes/:class_id/students', to: "classes#show", as: "classes_show"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
--- test/controllers/classes_controller_test.rb
+++ test/controllers/classes_controller_test.rb
@@ -1,13 +1,54 @@
 require "test_helper"
 
 class ClassesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @school = schools(:one)
+    @course = courses(:one)
+    @student = students(:one)
+  end
+  
   test "should get index" do
-    get classes_index_url
+    get classes_index_url(@school)
     assert_response :success
   end
 
+  test "get index should respond with proper json" do
+    expected_json = {
+      data: [
+        {id: 1, number: 1, letter: "А", students_count: 1},
+        {id: 2, number: 1, letter: "Б", students_count: 1}
+      ]
+    }
+    get classes_index_url(@school)
+    assert expected_json == json_response, "Actual json response is incorrect"
+  end
+
   test "should get show" do
-    get classes_show_url
+    get classes_show_url(@school, @course)
     assert_response :success
   end
+  
+  test "get show should respond with proper json" do
+    expected_json = {
+      data: [
+        {
+          id: 1,
+          first_name: "test-first-name", 
+          last_name: "test-last-name",
+          surname: "test-surname",
+          class_id: 1,
+          school_id: 1
+        }
+      ]
+    }
+    
+    get classes_show_url(@school, @course)
+    assert expected_json == json_response, "Actual json response is incorrect"
+  end
+
+  private
+    def json_response
+      (ActiveSupport::JSON.decode @response.body).deep_symbolize_keys
+    end
+    
 end
